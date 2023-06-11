76aa1692394accd36863b5126f47f284295d04f1
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2023-06-11 18:39:39 +0300

    v.1.0

--- app/src/main/java/com/tahacinar/calculator/MainActivity.kt
+++ app/src/main/java/com/tahacinar/calculator/MainActivity.kt
@@ -2,9 +2,13 @@ package com.tahacinar.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import com.tahacinar.calculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.ArithmeticException
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +31,18 @@ class MainActivity : AppCompatActivity() {
     fun onEqualClick(view: View) {}
 
 
-    fun onDigitClick(view: View) {}
+    fun onDigitClick(view: View) {
+        if (stateError){
+            binding.dataTv.text = (view as Button).text
+            stateError = false
+
+        } else {
+            binding.dataTv.append((view as Button).text)
+        }
+
+        lastNumeric = true
+        onEqual()
+    }
 
 
     fun onOperatorClick(view: View) {}
@@ -37,5 +52,27 @@ class MainActivity : AppCompatActivity() {
 
 
     fun onClearClick(view: View) {}
+
+    fun onEqual(){
+
+        if (lastNumeric && !stateError) {
+            val txt = binding.dataTv.text.toString()
+            expression = ExpressionBuilder(txt).build()
+
+            try{
+                val result = expression.evaluate()
+                binding.resultTv.visibility = View.VISIBLE
+                binding.resultTv.text = "=" + result.toString()
+
+            }catch (ex : ArithmeticException){
+                Log.e("evaluate error",ex.toString())
+                binding.resultTv.text = "Error"
+                stateError = true
+                lastNumeric = false
+
+
+            }
+        }
+    }
 
 }
